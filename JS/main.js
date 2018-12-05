2bb48b0db33ef7ef5464aee5632f84938f41e2d6
Keyon Jerome <keyonjerome@gmail.com>
2018-12-04 22:28:42 -0500

    Smooth scroll added.

---
+++ JS/main.js
@@ -0,0 +1,12 @@
+$(function() {
+  // Add background color to nav when user scrolls more than 50px down
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 50) {
+      $("#main-navbar").removeClass("navbar-dark");
+      $("#main-navbar").addClass("scrolledNavbar navbar-light");
+    }
+    if ($(this).scrollTop() < 50) {
+      $("#main-navbar").removeClass("scrolledNavbar navbar-light");
+      $("#main-navbar").addClass("navbar-dark");
+    }
+  });
